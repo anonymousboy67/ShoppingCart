ecb96c67ed59cd056f6b2e46e1601ea960675d93
anonymousboy67 <aashishad67@gmail.com>
2024-03-03 22:39:37 +0545

    ShoppingCartContext

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,17 +1,30 @@
-import {Card} from "react-bootstrap"
-import { Store } from '../pages/Store';
+import { Button, Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
-type StoreItemProps={
-    id:number;
-    name:string;
-    price:number;
-    imgUrl:string;
-}
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    imgUrl: string
+  }
 
-export function StoreItem({id,name,price,imgUrl}:StoreItemProps){
-    return(
-        <Card>
-            <Card.Img variant="top" src={imgUrl} height="200px"/>
-        </Card>
+  export function StoreItems({ id, name, price, imgUrl }: StoreItemProps) {
+
+    return (
+      <Card className="h-100">
+        <Card.Img
+          variant="top"
+          src={imgUrl}
+          height="200px"
+          style={{ objectFit: "cover" }}
+        />
+        <Card.Body className="d-flex flex-column">
+          <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+            <span className="fs-2">{name}</span>
+            <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+          </Card.Title>
+          <Button key={id}>Hello</Button>
+        </Card.Body>
+      </Card>
     )
-}
+  }
