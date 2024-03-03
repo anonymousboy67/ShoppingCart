9e9b0c6533707659887019ec8f1fb525cc24b0ea
anonymousboy67 <aashishad67@gmail.com>
2024-03-03 21:39:28 +0545

    Store Page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,16 @@
+import storeItems from "../data/items.json"
+import { Col, Row } from "react-bootstrap"
+
 export function Store(){
     return(
-        <h1>About</h1>
+        <>
+        <h1>Store</h1>
+        <Row>
+            {storeItems.map(item=>(
+                <Col>{JSON.stringify(item)}</Col>
+            ))}
+        </Row>
+        
+        </>
     )
 }
