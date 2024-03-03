458a432e9ef18f8231883de455cca12dba9a4e7f
anonymousboy67 <aashishad67@gmail.com>
2024-03-03 22:08:23 +0545

    FormatCurrency

---
+++ src/components/StoreItems.tsx
@@ -0,0 +1,17 @@
+import {Card} from "react-bootstrap"
+import { Store } from '../pages/Store';
+
+type StoreItemProps={
+    id:number;
+    name:string;
+    price:number;
+    imgUrl:string;
+}
+
+export function StoreItem({id,name,price,imgUrl}:StoreItemProps){
+    return(
+        <Card>
+            <Card.Img variant="top" src={imgUrl} height="200px"/>
+        </Card>
+    )
+}
