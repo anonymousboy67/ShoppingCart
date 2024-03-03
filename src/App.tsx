dabacf62ae132355a7688801b60b1e2feb492e23
anonymousboy67 <aashishad67@gmail.com>
2024-03-03 22:46:57 +0545

    Route Path

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 function App() {
-  
-
   return (
-
-  <>
-  <Navbar/>
-  <Container className="mb-4">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </Container>
-  </>
-
-
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
